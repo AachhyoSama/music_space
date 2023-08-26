f8c52987a36ff1c241bdf657a551b5b570da8277
Anish Gurung <anish.gurung@yipl.com.np>
2023-08-26 09:38:54 -0700

    post request handled

--- music_space_api/urls.py
+++ music_space_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from music_space_api.views import RoomView
+from music_space_api.views import RoomView, CreateRoomView
 
 urlpatterns = [
     path("room", RoomView.as_view()),
+    path("create-room", CreateRoomView.as_view()),
 ]
--- music_space_api/views.py
+++ music_space_api/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        # Check if the user logged in has an active session
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        # Check if the serializer is valid
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get("guest_can_pause")
+            votes_to_skip = serializer.data.get("votes_to_skip")
+            host = self.request.session.session_key
+
+            # Check if the host already exists in database
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                # If the host exists, just update the data
+
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=["guest_can_pause", "votes_to_skip"])
+
+            else:
+                # Create a new room
+                room = Room.objects.create(
+                    host=host,
+                    guest_can_pause=guest_can_pause,
+                    votes_to_skip=votes_to_skip,
+                )
+                room.save()
+
+            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
